e284418cb27cea0400a50916e19b024b64697344
Douglas Rocha <douglas_igt14@hotmail.com>
2023-12-27 02:01:51 -0300

    WIP

--- src/app/components/admin/hotelsList.tsx
+++ src/app/components/admin/hotelsList.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { Box, Divider, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material';
 import { styleHotelsList, styleTextList } from '@/app/styles/styles';
-import { SafetyDivider } from '@mui/icons-material';
+import { env } from "../../env";
 
 const HotelLandingPage = ({ hotels }) => {
   const [selectedDate, setSelectedDate] = useState(null);
@@ -23,7 +23,28 @@ const HotelLandingPage = ({ hotels }) => {
                 <List>
 
                   <ListItem key={index}>
-                      <ListItemText secondary="Quartos Disponíveis"><span style={styleTextList}>{hotel.name}</span> </ListItemText>
+                      <ListItemText secondary="Quartos Disponíveis">
+
+                      <Grid container spacing={2} 
+                          justifyContent="space-between"
+                           alignItems="center"
+                          width="100%">
+                        <Grid item xs={11}>
+                          <span style={styleTextList}>{hotel.name}</span> 
+                        </Grid>
+                      {(
+                         hotel.image_url ? 
+                            <Grid item xs={1} justifyContent="flex-end">
+                            <img
+                              src={`${env.backend}/${hotel.image_url}`}
+                              alt={hotel.name}
+                              style={{ maxWidth: '100%', maxHeight: '80px' }}
+                              loading="lazy"/>
+                              </Grid>
+                            : null
+                         )}
+                         </Grid>
+                      </ListItemText>
                   </ListItem>
                 </List>
                 <Divider />
